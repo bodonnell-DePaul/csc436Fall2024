c61e00b4c793abc2d47a2e2da4b2affe261663db
Brian O'Donnell <brian.m.odonnell@gmail.com>
2024-09-24 01:45:53 +0000

    use state / use effect

--- src/TopicList.js
+++ src/TopicList.js
@@ -2,17 +2,67 @@ import Accordion from 'react-bootstrap/Accordion';
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import TopicThread from './TopicThread';
-import topics from './dummyData.json';
+import topicData from './dummyData.json';
 import commentData from './commentData.json';
 import './TopicList.css';
+import Badge from 'react-bootstrap/Badge';
+import Form from 'react-bootstrap/Form';
+import { useState, useEffect } from 'react';
+
+
 
 function TopicList() {
+
+// "id": 1,
+//         "title": "My Awesome First Title",
+//         "content": "Even Better Content!!!",
+//         "rating": 4
+  function saveTopic(e, data) {
+    console.log(e)
+    topics.concat({
+      "id":commentData.length + 1,
+      "title":e.title,
+      "content":e.content,
+      "rating":0
+    });
+    console.log(topics);
+
+  }
+
+  const [comments, setComments ] = useState(commentData)
+  const [topics, setTopics ] = useState(topicData)
+
+  useEffect(() => {
+    setComments(comments)
+    setTopics(topics);
+  });
+
   return (
+    <div>
+    <Form>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Label>Topic Subject</Form.Label>
+        <Form.Control type="text" placeholder="Topic Content" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+        <Form.Label>Topic Content</Form.Label>
+        <Form.Control as="textarea" rows={3} />
+      </Form.Group>
+          <Button variant="primary" type='submit' onClick={saveTopic} >
+            Save Changes
+        </Button>
+    </Form>
     <Accordion defaultActiveKey="0">
       {topics.map(
           (topic, index) => (
             <Accordion.Item eventKey={index.toString()} key={index}>
-              <Accordion.Header>{topic.title}</Accordion.Header>
+              <Accordion.Header>{topic.title}
+                <Badge bg="primary" pill>
+                  {
+                    comments.filter(comment => comment.title_id === topic.id).length
+                  }
+                </Badge>
+              </Accordion.Header>
               <Accordion.Body>
                 {topic.content}
               <TopicThread comments={commentData.filter(comment => comment.title_id === topic.id)} />
@@ -28,6 +78,7 @@ function TopicList() {
         )
       }
     </Accordion>
+    </div>
   );
 }
 
--- src/TopicThread.js
+++ src/TopicThread.js
@@ -1,16 +1,21 @@
-
+import { ListGroupItem } from 'react-bootstrap';
+import ListGroup from 'react-bootstrap/ListGroup';
 
 function TopicThread({comments}){
 
     const matchedComments = comments.map((chat, index) => {
         return(
-            <div>
-                <strong>{chat.title}</strong>
-                <p>{chat.content}</p>
-            </div>
+            <ListGroup as="ul">
+                <ListGroup.Item as="li" className="d-flex justify-content-between align-items-start">
+                    <div className="ms-2 me-auto">
+                    <div className="fw-bold">{chat.title}</div>
+                    <p>{chat.content}</p>
+                    </div>
+                </ListGroup.Item>
+            </ListGroup>
+
         );
     });
-
 
     return(
         <div>
